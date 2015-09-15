9aa795576b0c1f3698cddd146663823a546d0057
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2015-09-15 15:21:17 +0200

    Simple tester for rotary dial cables

---
+++ tester.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+
+class Tester
+  def __init__(self):
+    board_mode()
+    setup()
+    light()
+
+  def board_mode(self):
+    GPIO.setmode(GPIO.BOARD)
+    self.in_to_out = {15: 22, 16: 23, 18: 24}
+
+  def bcm_mode(self):
+    GPIO.setmode(GPIO.BCM)
+    self.in_to_out = {22: 25, 24: 11, 24: 8}
+
+  def setup(self):
+    for p_in, p_out in self.in_to_out.iteritems():
+      GPIO.setup(p_in, GPIO.IN)
+      GPIO.setup(p_out, GPIO.OUT)
+
+  def light(self):
+    for p_in, p_out in self.in_to_out.iteritems():
+      GPIO.output(p_out, not GPIO.input(p_in))
+
+  def test(self):
+    while True:
+      light()
+
+  def cleanup(self):
+    GPIO.cleanup()    
+
+    
+tester = Tester()
+
+tester.test()
+tester.cleanup()
+
