2079f9869c04504f4dd38419647dc00cafc72b41
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2015-09-16 15:26:19 +0200

    Version of the counter with timing tables

--- counter.py
+++ counter.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 from threading import Timer
 import time
+import atexit
+from collections import OrderedDict
 
 # IN  | OUT | Color
-# 15  | 22  |  
+# 15  | 22  | Blue 
 # 16  | 23  | Green
-# 18  | 24  |
+# 18  | 24  | Red
 
 class Counter:
   pin_rotating = 16
@@ -15,6 +17,8 @@ class Counter:
 
   pout_rotating = 23
   pout_pulse    = 24
+
+  rotations = 0
 
   def __init__(self):
     GPIO.setmode(GPIO.BOARD)
@@ -28,26 +32,54 @@ class Counter:
     GPIO.add_event_detect(self.pin_rotating, GPIO.BOTH,    callback = self.Rotation)
     GPIO.add_event_detect(self.pin_pulse,    GPIO.FALLING, callback = self.NumberCounter)
 
+    self.StartCounting()
 
   def Rotation(self, channel):
     self.rotating = not GPIO.input(channel)
     GPIO.output(self.pout_rotating, self.rotating)
 
     if self.rotating:
-      print "\nStarted rotation"
+      # print "\nStarted rotation"
+      self.triggers[time.time()] = "Rotating START"
     else:
-      print "\nFinished rotation: %d" % self.rotations
+      self.triggers[time.time()] = "Rotating FINISH"
+
+      r = self.rotations
+      number = r / 2 + r % 2
+      print "\nFinished rotation: %d" % self.rotations 
+      print "Number: %d" % (number)
+
+      Timer(0.5, self.Timing).start()
 
     self.rotations = 0
    
     
   def NumberCounter(self, channel):
-    self.rotations += 1
-
+    self.triggers[time.time()] = "Pulse"
     if self.rotating:
+      self.rotations += 1
       print('.'),
     else:
       print('x'),
+
+
+  def StartCounting(self):
+    self.start_time = time.time()
+    self.triggers = OrderedDict()
+
+  def Timing(self):
+    'Print timing of triggers captured'
+ 
+    print "\n---\n"
+    prev = 0
+
+    for tt, trigger in self.triggers.iteritems():
+      t = int(round( (tt - self.start_time) * 1000 ))
+      print "%16s | +%6dms | %10dms " % (trigger, t-prev, t) 
+      prev = t
+      time.sleep(0.01) # For some strange reason sometimes flushing doesn't work correctly :/ adding this delay to help it print properly.
+
+    self.StartCounting()
 
   def cleanup(self):
     GPIO.cleanup()    
@@ -55,6 +87,11 @@ class Counter:
 
 c = Counter()
 
+def cleanup():
+  print "Cleaning up"
+  GPIO.cleanup()    
+
+atexit.register(cleanup)
 
 while True:
   time.sleep(1)
