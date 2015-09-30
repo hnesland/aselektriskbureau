c4a40a3c0812be1f0c716582c6d8736e8822c976
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2015-09-30 10:49:17 +0200

    Stargin on debouncing tests + general cleanup

--- rio.py
+++ rio.py
@@ -150,26 +150,30 @@ class RioTest:
         IN[13]    OUT[11]
         o         o
         |   ___   |
-        \--|___|--o----\
-           1kOhm  |    |
-                 .-.  ___
-           10kOhm| |  \ /  LED Diode (with ~270 Ohm resistor) [optional]
-                 '-'  ---
-                  |    |
-                  o----/
+        \--|___|--o
+          10kOhm  |
+                 .-.
+           270Ohm| |
+                 '-'
+                  |
+                 ___
+                 \ /  LED Diode (with ~270 Ohm resistor)  is optional
+                 ---
                   |
                   o GND[9]
 
         This way, when OUT is pulled HIGH the input should also register HIGH state, and if the output is not powered
         the input should also be in LOW state.
 
-        The 1kOhm resistor is meant to protect the chip in case pins are setup incorrectly and 10k Ohm resistor is meant
-         to limit current flowing through the circuit ( Lookup Pull-Down/Pull-Up conecept here:
-         http://elinux.org/RPi_Tutorial_EGHS:Switch_Input ).
+        The 10kOhm resistor is meant to protect the chip in case pins are setup incorrectly and limit current flowing
+        through the circuit. Diode with current limiting resistor can be added to have visual indicator of pin state
+        and correctness of the connection, although it'll only light up for a second at the start of the test.
 
         You can optionally add a diode with resistor in series to limit current, which resistor exactly you need you can
          learn here: http://www.instructables.com/id/Choosing-The-Resistor-To-Use-With-LEDs/?ALLSTEPS
     """
+
+    minimal_change_duration = 5  # ms.
 
     def __init__(self, **pins):
         """
@@ -193,17 +197,16 @@ class RioTest:
     def result(expected, actual):
         ok = '\033[92m'
         fail = '\033[91m'
-	exp = '\033[93m'
+        exp = '\033[93m'
         end = '\033[0m'
 
         if expected == actual:
             print ok, actual, end
         else:
-            print fail, actual, end
-            print "Expected", exp, expected, end
+            print fail, actual, end, "Expected", exp, expected, end
 
     def test_regular(self):
-        rin  = Rin.get(self.pin)
+        rin = Rin.get(self.pin)
         rout = Rout.get(self.pout)
 
         print 'HIGH for 1 second (check if led is on).'
@@ -215,26 +218,26 @@ class RioTest:
         time.sleep(0.01)
         self.result("HIGH", rin.text_state)
 
-        print 'LOW  (10ms delay)...',
+        print 'LOW  (10ms delay) ...',
         rout.low()
         time.sleep(0.01)
         self.result("LOW", rin.text_state)
 
-        print 'HIGH (1ms delay) ...',
+        print 'HIGH (1ms delay) ....',
         rout.high()
         time.sleep(0.001)
         self.result("HIGH", rin.text_state)
 
-        print 'LOW  (1ms delay)...',
+        print 'LOW  (1ms delay) ....',
         rout.low()
         time.sleep(0.001)
         self.result("LOW", rin.text_state)
 
-        print 'HIGH (no delay)...',
+        print 'HIGH (no delay) .....',
         rout.high()
         self.result("HIGH", rin.text_state)
 
-        print 'LOW  (no delay)...',
+        print 'LOW  (no delay) .....',
         rout.low()
         self.result("LOW", rin.text_state)
 
@@ -249,23 +252,22 @@ class RioTest:
             time.sleep(sleep_ms / 1000.0)
 
     def test_callbacks(self):
-        print '\nSetting up callback tests'
+        delay = self.minimal_change_duration
+
+        print '\nStarting callback tests\n'
 
         self.rin.changed = self.changed
         self.rin.rising = self.rising
         self.rin.falling = self.falling
 
-        delay = 5 #ms
+        print "Raise. 100ms delay. Fall. 100ms delay.",
 
-        print "\nRaise 100ms delay."
         self.high(100)
-
-        print "\nFall  100ms delay."
         self.low(100)
 
         self.result(['Rising', 'Changed', 'Falling', 'Changed'], self.call_stack)
 
-        print "\nRaise, Fall, Raise (%dms delay)" % delay
+        print "Raise, Fall, Raise (%dms delay)" % delay,
         self.call_stack = []
 
         self.high(delay)
@@ -274,7 +276,7 @@ class RioTest:
 
         self.result(['Rising', 'Changed', 'Falling', 'Changed', 'Rising', 'Changed'], self.call_stack)
 
-        print '\nFall, Raise, Fall (%dms delay)' % delay
+        print 'Fall, Raise, Fall (%dms delay)' % delay,
         self.call_stack = []
 
         self.low(delay)
@@ -282,6 +284,22 @@ class RioTest:
         self.low(delay)
 
         self.result(['Falling', 'Changed', 'Rising', 'Changed', 'Falling', 'Changed'], self.call_stack)
+
+    def test_debouncing(self):
+        delay = self.minimal_change_duration
+
+        self.rin.bounce_interval = 20  # ms
+
+        print '\nStarting debouncing tests\n'
+
+        print "Raise, Fall, Raise (%dms delay)" % delay,
+        self.call_stack = []
+
+        self.high(delay)
+        self.low(delay)
+        self.high(delay)
+
+        self.result(['Rising', 'Changed'], self.call_stack)
 
     def rising(self, current_time, state_duration):
         self.rising_called = {'current_time': current_time, 'state_duration': state_duration}
@@ -301,6 +319,7 @@ if __name__ == "__main__":
     tester = RioTest(pin=13, pout=11)
     tester.test_regular()
     tester.test_callbacks()
+    tester.test_debouncing()
 
     print '\n\nWaiting one second for all tests to finish and timer to stop'
     time.sleep(1)
